c3505c5651ec7443e52ac1f8befdffac15c3019d
Yemi <yemiadebayo60@gmail.com>
2019-05-02 15:04:41 +0100

    Allow docks to store bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,12 @@
 require_relative 'Bike'
 
 class DockingStation
+  attr_reader :bike
+
+  def dock(bike)
+     @bike = bike
+  end
+
   def release_bike
     Bike.new
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,12 +3,54 @@ require 'docking_station'
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
+  it {is_expected.to respond_to(:dock).with(1).argument}
 
   it 'release bike' do
-     bike = subject.release_bike
-     expect(bike).to be_working
+     expect(subject.release_bike).to be_working
+  end
+
+  it 'an instance of a bike docked' do
+     bike = Bike.new
+     expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'docking station can return a bike' do
+    expect(subject).to respond_to :bike
   end
 end
 
+# it 'new test for docking station to return bike' do
+#   bike = Bike.new
+#   subject.dock(bike)
+#   expect(subject.bike).to eq bike
+# end
 
-# it {is_expected.to respond_to(:dock).with(1).argument}
+
+
+
+# require '/Users/yems/Projects/Boris-Bikes/lib/docking_station.rb'
+#
+# require '/Users/yems/Projects/Boris-Bikes/lib/bike.rb'
+#
+#  bike = Bike.new
+#  docking_station = DockingStation.new
+# docking_station.dock(bike)
+# docking_station.bike
+
+
+
+
+
+# it {is_expected.to respond_to :black}
+#
+# it {is_expected.to respond_to(:black).with(2).arguments}
+# it {is_expected.to respond_to(:black).with(1).argument}
+# it {is_expected.to respond_to(:white).with(2).arguments}
+# it {is_expected.to resppond_to :black}
+#
+#
+# expect(5).to eql (6)
+#
+# it {is_expected.to respond_to :black}
+#
+# it {is_expected.to respond_to(:black).with(2).arguments}
